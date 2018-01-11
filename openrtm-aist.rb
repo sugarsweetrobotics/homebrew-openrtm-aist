ada2dc2e758d89c2cff19aa099761a66752cf771
ysuga <ysuga@ysuga.net>
2018-01-11 10:58:55 +0900

    download url change

--- openrtm-aist.rb
+++ openrtm-aist.rb
@@ -2,7 +2,8 @@ require "formula"
 
 class OpenrtmAist < Formula
   homepage "http://openrtm.org"
-  url "https://github.com/sugarsweetrobotics/homebrew-openrtm-srcs/blob/master/OpenRTM-aist-1.1.2.tar.gz"
+
+  url "https://github.com/sugarsweetrobotics/homebrew-openrtm-srcs/raw/master/OpenRTM-aist-1.1.2.tar.gz"
   sha256 "8dde5a89d55948e70882e475770fc6b521fa66034911abf0810b989a275fb2dd"
   head "https://svn.openrtm.org/OpenRTM-aist/tags/RELEASE1.1.2"
   version "1.1.2"
