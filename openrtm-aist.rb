1bd73d145bf614d83887512073bc5c37af99faed
ysuga <ysuga@ysuga.net>
2018-01-11 15:19:15 +0900

    udpate url

--- openrtm-aist.rb
+++ openrtm-aist.rb
@@ -26,7 +26,7 @@ class OpenrtmAist < Formula
 
   bottle do
     root_url "https://raw.githubusercontent.com/sugarsweetrobotics/homebrew-openrtm-aist-bottles/master"
-    sha256 "12bef50482c15fd679fab8c7d8a740294cb8ee866b333604b472973d2d0d8936" => :high_sierra
+    sha256 "5bd2bffc50dc26b5cd0547ed5f7979f81278cba66a078a21c5587a20f03dfabf"
   end
 
   def pour_bottle?
