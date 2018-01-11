7b439680fb342e62894f1424e40f00661f49cd3f
ysuga <ysuga@ysuga.net>
2018-01-11 10:55:26 +0900

    change name

--- openrtm-aist.rb
+++ openrtm-aist.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class OpenRTM < Formula
+class OpenrtmAist < Formula
   homepage "http://openrtm.org"
   url "https://github.com/sugarsweetrobotics/homebrew-openrtm-srcs/blob/master/OpenRTM-aist-1.1.2.tar.gz"
   sha256 "35f03c0e60ed5c05ccd46a264c6613b98fb1d1b242c1721536bd52f2ff740ba1"
