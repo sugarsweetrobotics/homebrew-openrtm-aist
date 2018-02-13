afc4e759288c775a90fad12bf52e927bf49df620
ysuga <ysuga@ysuga.net>
2018-02-13 12:40:53 +0900

    add openrtm-aist-python

---
+++ openrtm-aist-python.rb
@@ -0,0 +1,35 @@
+require "formula"
+
+class OpenrtmAistPython < Formula
+  homepage "http://openrtm.org"
+
+  url "https://github.com/sugarsweetrobotics/homebrew-openrtm-srcs/raw/master/OpenRTM-aist-Python-1.1.2.tar.gz"
+  sha256 "601e45ccc49ab2099d8bb6c01ae4b2cf7da79422c96b7e00f68b9492438852b1"
+  #  head "https://svn.openrtm.org/OpenRTM-aist/tags/RELEASE1.1.2"
+  version "1.1.2"
+
+  depends_on "omniorb-python"
+
+  def install
+    #    system "./configure", "--prefix=#{prefix}"
+    # system "./configure"
+    # system "make"
+    # system "make", "install"
+
+    system "python", "setup.py", "install"
+
+    # bin.install 'bin'
+    # share.install 'share'
+    # lib.install 'lib'
+  end
+
+
+  #  bottle do
+  #  root_url "https://raw.githubusercontent.com/sugarsweetrobotics/homebrew-openrtm-aist-bottles/master"
+  #  sha256 "8d33d9f98bc8a1f7310aa92d7bd2827bdc6fd224f8b9b281e5902c310307c285" => :high_sierra
+  # end
+
+  #  def pour_bottle? do
+  #   return true
+  # end
+end
