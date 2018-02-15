fbe69373a34d417455983263c1d6574381a94cb1
ysuga <ysuga@ysuga.net>
2018-02-15 11:32:43 +0900

    remove version depend

--- openrtp.rb
+++ openrtp.rb
@@ -8,7 +8,7 @@ class Openrtp < Formula
   sha256 "b31c5274d61a3a760a9f5cfc956c30a074ea9ba6c7f1940d2261c6f901d3525a"
   version "1.1.2"
 
-  depends_on macos: '>= :leopard'
+  #  depends_on macos: '>= :leopard'
 
   app 'OpenRTP.app', target: 'OpenRTP-1.1.2.app'
   #  def pour_bottle? do
