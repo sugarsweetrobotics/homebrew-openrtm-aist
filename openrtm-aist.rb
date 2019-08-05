7f2d46fbbeae4e67df2094de024d732a9c828b7c
ysuga <ysuga@ysuga.net>
2019-08-05 13:59:33 +0900

    version 1.2.0

--- openrtm-aist.rb
+++ openrtm-aist.rb
@@ -3,10 +3,10 @@ require "formula"
 class OpenrtmAist < Formula
   homepage "http://openrtm.org"
 
-  url "https://github.com/sugarsweetrobotics/homebrew-openrtm-srcs/raw/master/OpenRTM-aist-1.1.2.tar.gz"
-  sha256 "8b16e5ad765d957b99f723cc42c7e7fed3a5b612e86f72f5a8dc7dd0d202146c"
-  head "https://svn.openrtm.org/OpenRTM-aist/tags/RELEASE1.1.2"
-  version "1.1.2"
+  url "https://github.com/sugarsweetrobotics/homebrew-openrtm-srcs/raw/master/OpenRTM-aist-1.2.0.tar.gz"
+  sha256 "4eb83d2d196e57a447a169e5207ac4d3f2050fd1b790d4df253de0cf444a328f"
+  head "https://github.com/sugarsweetrobotics/OpenRTM-aist/tree/svn/RELENG_1_2"
+  version "1.2.0"
 
   depends_on "cmake"
   depends_on "libtool"
@@ -25,10 +25,10 @@ class OpenrtmAist < Formula
   end
 
 
-  bottle do
-    root_url "https://raw.githubusercontent.com/sugarsweetrobotics/homebrew-openrtm-aist-bottles/master"
-    sha256 "8d33d9f98bc8a1f7310aa92d7bd2827bdc6fd224f8b9b281e5902c310307c285" => :high_sierra
-  end
+  #bottle do
+  #  root_url "https://raw.githubusercontent.com/sugarsweetrobotics/homebrew-openrtm-aist-bottles/master"
+  #  sha256 "8d33d9f98bc8a1f7310aa92d7bd2827bdc6fd224f8b9b281e5902c310307c285" => :high_sierra
+  #end
 
   #  def pour_bottle? do
   #   return true
