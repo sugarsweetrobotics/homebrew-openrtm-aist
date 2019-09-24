f3df25e7b3e275bdfd84ac12844b24befeab0bb5
ysuga <ysuga@ysuga.net>
2019-09-24 21:25:22 +0900

    update openrtp

--- Casks/openrtp.rb
+++ Casks/openrtp.rb
@@ -1,11 +1,10 @@
 cask 'openrtp' do 
-  homepage "http://openrtm.org"
+  version "1.2.0"
+  sha256 "be2b6e1194e7721b1bcf6c4f960e7ffc9403a038fa0c3149f938b9a3210db2c4"
 
   url "http://sugarsweetrobotics.com/pub/OpenRTM-aist/OpenRTP-1.2.0.dmg"
-  sha256 "be2b6e1194e7721b1bcf6c4f960e7ffc9403a038fa0c3149f938b9a3210db2c4"
-  version "1.2.0"
+  name 'OpenRTP'
+  homepage "http://openrtm.org"
 
-  # depends_on macos: '>= :leopard'
-  app 'Eclipse.app'
-
+  app 'Eclipse.app', target: 'OpenRTP 1.2.0.app'
 end
