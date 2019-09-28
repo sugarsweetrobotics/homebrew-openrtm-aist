6d1db4db3c7040cc6c4a786704f960f181edab35
Yuki Suga <ysuga@ysuga.net>
2019-09-28 11:52:01 +0900

    add new version dmg

--- Casks/openrtp.rb
+++ Casks/openrtp.rb
@@ -2,7 +2,7 @@ cask 'openrtp' do
   version "1.2.0"
   sha256 "96566910da716feefba0024a1542f943b0eb44456e93f7a319e1648be2d2a78e"
 
-  url "http://sugarsweetrobotics.com/pub/OpenRTM-aist/openrtp-1.2.0.dmg"
+  url "http://sugarsweetrobotics.com/pub/OpenRTM-aist/openrtp_120.dmg"
   name 'OpenRTP'
   homepage "http://openrtm.org"
 
