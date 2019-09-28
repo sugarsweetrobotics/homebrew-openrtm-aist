79e4b555095e0cf20dd07a3fd254ad5bd4e19a58
Yuki Suga <ysuga@ysuga.net>
2019-09-28 15:29:39 +0900

    update shasum

--- Casks/openrtp.rb
+++ Casks/openrtp.rb
@@ -1,7 +1,9 @@
 cask 'openrtp' do 
   version "1.2.0"
   sha256 "91c12bed755cc230779ae9a66ce85e4753df5bda732ce5281d00176fdae524c8"
-  url "http://sugarsweetrobotics.com/pub/OpenRTM-aist/openrtp_120.dmg"
+  sha256 "17803ef1a5f7eb0cf0bf9c02ba6e43d544158a042f80c20c488c53391e02448f"
+  #url "http://sugarsweetrobotics.com/pub/OpenRTM-aist/openrtp_120.dmg"
+  url "http://sugarsweetrobotics.com/pub/OpenRTM-aist/openrtp_120.tar.bz2"
   name 'OpenRTP'
   homepage "http://openrtm.org"
 
