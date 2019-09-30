d0e369e169a1316320412ae72888ff208a9ee944
ysuga <ysuga@ysuga.net>
2019-09-30 20:12:50 +0900

    add curl strategy

--- Casks/openrtp.rb
+++ Casks/openrtp.rb
@@ -2,8 +2,7 @@ cask 'openrtp' do
   version "1.2.0"
   #sha256 "91c12bed755cc230779ae9a66ce85e4753df5bda732ce5281d00176fdae524c8"
   sha256 "b08502d54206a5779f4f7cdb34a889e6f39a4993159902b004f886f70bf762d6"
-  #url "http://sugarsweetrobotics.com/pub/OpenRTM-aist/openrtp_120.dmg"
-  url "http://sugarsweetrobotics.com/pub/OpenRTM-aist/openrtp120.zip", using: CurlDownloadStrategy
+  url "http://sugarsweetrobotics.com/pub/OpenRTM-aist/openrtp120.zip", :using => :curl
   name 'OpenRTP'
   homepage "http://openrtm.org"
 
