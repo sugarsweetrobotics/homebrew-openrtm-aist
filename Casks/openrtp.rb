c0d223508f0f7d09eb2ede1203cedb50adecd8e5
ysuga <ysuga@ysuga.net>
2019-09-30 20:54:23 +0900

    update ssl

--- Casks/openrtp.rb
+++ Casks/openrtp.rb
@@ -1,7 +1,7 @@
 cask 'openrtp' do 
   version "1.2.0"
   sha256 "e9e985307a2a30de955f5859d0f7bdee1b3b7ab25689ae6e159b35be430efecf"
-  url "http://sugarsweetrobotics.com/pub/OpenRTM-aist/openrtp_120.dmg", :using => :curl
+  url "https://sugarsweetrobotics.com/pub/OpenRTM-aist/openrtp_120.dmg", :using => :curl
   name 'OpenRTP'
   homepage "http://openrtm.org"
 
